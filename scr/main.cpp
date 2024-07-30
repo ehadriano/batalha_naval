97f6a6f53a05aa583a6fd50f82586e7952d717df
Eduardo Hubner Adriano <ehadriano@ufmg.br>
2024-07-30 00:33:05 -0300

    Testa Limites Carrier Horizontal

--- scr/main.cpp
+++ scr/main.cpp
@@ -5,20 +5,17 @@
 
 using namespace std;
 
-//Testa movimentação na Horizontal
-int main(){
+int main(){ //Testa limites na movimentação para Carrier Horizontal
 
     cursor a;
     jogador b;
     int q1=2, w1=3;
-    int q2=2, w2=10; //coodenadas de AO no Tabuleiro 2
+    int q2=2, w2=10; //coodenadas de AO no Tabuleiro 2 (Não tenho ideia de como ela começa em 2,10 e não em 2,28)
 
     gotoxy(0,0);
     a.printarTabuleiro();
     gotoxy(0,25);
     a.printarTabuleiro();
-
-    //.printar_Carrier(2,10,1);//Por algum motivo, A0 tem coordenada (2,10), não (2,28) como esperado
 
     while(true){
         b.mover_Carrier(q2,w2,1);
