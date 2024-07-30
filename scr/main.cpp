07d63d2a4d021ad8256b850a2863cec6beacb353
Eduardo Hubner Adriano <ehadriano@ufmg.br>
2024-07-29 23:53:47 -0300

    Testa um Carrier na posição Vertical (c==0)

--- scr/main.cpp
+++ scr/main.cpp
@@ -5,15 +5,20 @@
 
 using namespace std;
 
+//Teste do Carrier na Vertical c==0
 int main(){
 
     cursor a;
+    jogador b;
     int q1=2, w1=3;
+    int q2=2, w2=28; //coodenadas de AO no Tabuleiro 2
 
     gotoxy(0,0);
     a.printarTabuleiro();
     gotoxy(0,25);
     a.printarTabuleiro();
+
+    b.printar_Carrier(2,10,0);//Por algum motivo, A0 tem coordenada (2,10), não (2,28) como esperado
 
     while(true){
         a.moverCursor(q1,w1);
