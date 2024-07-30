75454ca0f205fbf04c792c6be8a0f780591dd038
Eduardo Hubner Adriano <ehadriano@ufmg.br>
2024-07-29 23:46:13 -0300

    Declara os métodos da classe 'jogador'

---
+++ include/jogador.hpp
@@ -0,0 +1,11 @@
+#ifndef JOGADOR_HPP
+#define JOGADOR_HPP
+
+class jogador {
+public:
+    void printar_Carrier(int x, int y, int z);
+    void apagar_Carrier(int x, int y, int z);
+    void mover_Carrier(int &x, int &y, int z);
+};
+
+#endif // JOGADOR_HPP
