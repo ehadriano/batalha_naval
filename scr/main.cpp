b2b275c62e31c78eb9fac3a77daaed1d4aec8b7d
Eduardo Hubner Adriano <ehadriano@ufmg.br>
2024-07-30 22:56:44 -0300

    Testa as funções de rotação

--- scr/main.cpp
+++ scr/main.cpp
@@ -10,18 +10,18 @@ int main(){
     cursor a;
     jogador b;
     peca c = { {2,10}, { {2,0}, {4,0}, {6,0} }, 'P' }; //original, perifericos, char
+    peca v = { {2,10}, { {2,0}, {4,0}, {6,0} }, 'P' };
 
-    int q1=2, w1=3;
-    int q2=2, w2=10; //coodenadas de AO no Tabuleiro 2 (Não tenho ideia de como ela começa em 2,10 e não em 2,28)
-
-    b.printar(c);
-
-    /*
     gotoxy(0,0);
     a.printarTabuleiro();
     gotoxy(0,25);
     a.printarTabuleiro();
-    */
+
+    b.printar(v);
+
+    b.rotacionar(c);
+
+    b.printar(c);
 
 return 0;
 }
