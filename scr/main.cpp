c4fcda6deb0ee359cca9253cba8cf1efd74cd03c
Eduardo Hubner Adriano <ehadriano@ufmg.br>
2024-07-30 22:14:27 -0300

    Teste da nova jogador.cpp

--- scr/main.cpp
+++ scr/main.cpp
@@ -5,21 +5,23 @@
 
 using namespace std;
 
-int main(){ //Testa limites na movimentação para Carrier Vertical
+int main(){
 
     cursor a;
     jogador b;
+    peca c = { {2,10}, { {2,0}, {4,0}, {6,0} }, 'P' }; //original, perifericos, char
+
     int q1=2, w1=3;
     int q2=2, w2=10; //coodenadas de AO no Tabuleiro 2 (Não tenho ideia de como ela começa em 2,10 e não em 2,28)
 
+    b.printar(c);
+
+    /*
     gotoxy(0,0);
     a.printarTabuleiro();
     gotoxy(0,25);
     a.printarTabuleiro();
-
-    while(true){
-        b.mover_Carrier(q2,w2,0);
-    }
+    */
 
 return 0;
 }
