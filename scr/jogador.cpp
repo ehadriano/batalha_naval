d7663a9716dae8c54f14ea51dae045cd22bde1c4
Eduardo Hubner Adriano <ehadriano@ufmg.br>
2024-07-30 22:13:54 -0300

    Implementação da nova função da classe 'jogador'

--- scr/jogador.cpp
+++ scr/jogador.cpp
@@ -5,46 +5,11 @@
 
 using namespace std;
 
-//jogado.cpp
-void jogador::printar_Carrier(int x, int y, int c){ //Se c=0, Carrier na vertical
-    for (int i = 0; i < 4; ++i) {
-        if (c == 0) { // Vertical
-            gotoxy(x, y + 2 * i);
-        } else { // Horizontal
-            gotoxy(x + 2 * i, y);
-        }
-        cout << "C";
-    }
-}
-void jogador::apagar_Carrier(int x, int y, int c){ //Mesma lógica da função jogador::printar_Carrier, só que coloca um espaço vazio
-    for (int i = 0; i < 4; ++i) {
-        if (c == 0) { // Vertical
-            gotoxy(x, y + 2 * i);
-        } else { // Horizontal
-            gotoxy(x + 2 * i, y);
-        }
-        cout << " ";
-    }
-}
-/*Adiciona limites correspondentes ao Tabuleiro na movimentação*/
-void jogador::mover_Carrier(int &x, int &y, int c){
-    if(kbhit()){
-        jogador::apagar_Carrier(x,y,c);
-        char tecla = getch();
-        if(tecla == 'a' && x>2) x=x-2;
-        if(c==0){
-            if(tecla == 'd' && x<20) x=x+2;
-        }
-        if(c==1){
-            if(tecla == 'd' && x<14) x=x+2;
-        }
-        if(tecla == 'w' && y>10) y=y-2;
-        if(c==0){
-            if(tecla == 's' && y<21) y=y+2;
-        }
-        if(c==1){
-            if(tecla == 's' && y<28) y=y+2;
-        }
-        jogador::printar_Carrier(x,y,c);
+void jogador::printar(peca &P){
+
+    gotoxy(P.original.x, P.original.y); cout << P.D;
+
+    for(int i=0; i<3; i++){ //Controla o número de casas que um navio pode ocupar
+        gotoxy(P.original.x + P.perifericos[i].x, P.original.y + P.perifericos[i].y); cout << P.D;
     }
 }
