adf7b6d81c03186025d09418d54edadc737798ef
Eduardo Hubner Adriano <ehadriano@ufmg.br>
2024-08-01 22:37:49 -0300

    Implementa a função mover

--- scr/jogador.cpp
+++ scr/jogador.cpp
@@ -18,68 +18,54 @@ coord peris[4][3] = {{{2,0}, {4,0}, {6,0}},  //Carrier
 
 char navios[4] = {'C','T','D','S'}; //Representação de cada navio pela letra de sua inicial
 
-coord peca::posicao(int n) { //Posiciona um navio no tabuleiro
+coord peca::posicao(int n){ //Posiciona um navio no tabuleiro
+
     coord ret = {original.x, original.y};
-    if (n != 0) {
+    if(n != 0){
         ret.x += perifericos[n-1].x;
         ret.y += perifericos[n-1].y;
     }
-    return ret;
+return ret;
 }
+void jogador::printar(peca &P){
 
-void jogador::printar(peca &P) {
-    for (int i = 0; i < 4; i++) { //Controla o número de casas que um navio pode ocupar
+    for(int i=0; i<4; i++){ //Controla o número de casas que um navio pode ocupar
         coord c = P.posicao(i);
         ma[c.y][c.x] = P.D;
     }
-}
 
-void jogador::limpar(peca &P) {
-    for (int i = 0; i < 4; i++) { // Limpa a posição antiga do navio
-        coord c = P.posicao(i);
-        ma[c.y][c.x] = ' ';
-    }
 }
+coord jogador::rotacionar(coord &c){
 
-coord jogador::rotacionar(coord &c) {
-    coord ret = {c.y, c.x};
+    coord ret ={c.y,c.x};
+
     return ret;
 }
+void jogador::rotacionar(peca &P){
 
-void jogador::rotacionar(peca &P) {
-    for (int i = 0; i < 3; i++) {
+    for(int i=0; i<3; i++){
         P.perifericos[i] = jogador::rotacionar(P.perifericos[i]);
     }
-}
 
-void jogador::selecionar(peca &P, int r) { //Inicia todos e cada um dos Navios
+}
+void jogador::selecionar(peca &P, int r){ //Inicia todos e cada um dos Navios
+
     P.original.x = 2;
     P.original.y = 2;
     P.D = navios[r];
-    for (int i = 0; i < 3; i++) { //Para mais barcos, mudar o 3
-        P.perifericos[i] = peris[r][i];
+    for(int i=0; i<3; i++){ //Para mais barcos, mudar o 3
+            P.perifericos[i] = peris[r][i];
     }
 }
+void jogador::mover(peca &P){ //Lógica parecida com a já implementada em cursor::moverCursor
+     if(kbhit()){
 
-bool jogador::dentroDoTabuleiro(peca &P, char dir) {
-    for (int i = 0; i < 4; i++) {
-        coord c = P.posicao(i);
-        if (dir == 'a' && c.x <= 2) return false;
-        if (dir == 'd' && c.x >= 20) return false;
-        if (dir == 'w' && c.y <= 2) return false;
-        if (dir == 's' && c.y >= 19) return false;
-    }
-    return true;
-}
-
-void jogador::mover(peca &P) { //Lógica parecida com a já implementada em cursor::moverCursor
-    if (kbhit()) {
-        jogador::limpar(P);
         char tecla = getch();
-        if (tecla == 'a' && dentroDoTabuleiro(P, 'a')) P.original.x -= 2;
-        if (tecla == 'd' && dentroDoTabuleiro(P, 'd')) P.original.x += 2;
-        if (tecla == 'w' && dentroDoTabuleiro(P, 'w')) P.original.y -= 2;
-        if (tecla == 's' && dentroDoTabuleiro(P, 's')) P.original.y += 2;
+        if(tecla == 'a') P.original.x -=2;
+        if(tecla == 'd') P.original.x +=2;
+        if(tecla == 'w') P.original.y -=2;
+        if(tecla == 's') P.original.y +=2;
+
         jogador::printar(P);
-    }
+        }
 }
