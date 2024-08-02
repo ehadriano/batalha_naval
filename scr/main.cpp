adfad74456fe58486c5c3ee792f8ec6854b01753
Eduardo Hubner Adriano <ehadriano@ufmg.br>
2024-08-02 00:20:34 -0300

    Testa os dois novos métodos

--- scr/main.cpp
+++ scr/main.cpp
@@ -5,22 +5,22 @@
 
 using namespace std;
 
-/*Testa o método jogador::apagar e se a função de rotação
-está funcionado igual esperado*/
-int main() { 
-    cursor a;
-    jogador b;
+int main() {
+
+    jogador a;
+
     peca S;
     int r = 0; //r=0 Carrier, r=1 Tanker, r=2 Destroyer, r=3 Submarine
 
-    b.selecionar(S, r);
+    a.passarTabuleiro();
+    a.selecionar(S, r);
 
     gotoxy(0, 0);
-    a.printarTabuleiro();
+    a.desenharMapaju();
     while (true) {
-        b.mover(S);
+        a.mover(S);
         gotoxy(0, 0);
-        a.printarTabuleiro();
+        a.desenharMapaju();
     }
 
     return 0;
