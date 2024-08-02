e78ebf619fb34cbbbdec36b4b49a5bf9973379b3
Eduardo Hubner Adriano <ehadriano@ufmg.br>
2024-08-01 21:43:57 -0300

    Testa as funções 'limpar', 'mover' e 'dentroDoTabuleiro'

--- scr/main.cpp
+++ scr/main.cpp
@@ -5,26 +5,21 @@
 
 using namespace std;
 
-int main(){
-
+int main() {
     cursor a;
     jogador b;
-
     peca S;
-    int r = 0; //r=0 Carrier, r=1 Tanker, r=2 Destroyer, r=3 Submarine
+    int r = 2; //r=0 Carrier, r=1 Tanker, r=2 Destroyer, r=3 Submarine
 
-    b.selecionar(S,r);
+    b.selecionar(S, r);
 
-    //gotoxy(0,0);
-    //a.printarTabuleiro();
-
-    b.printar(S);
-    gotoxy(0,25);
+    gotoxy(0, 0);
     a.printarTabuleiro();
+    while (true) {
+        b.mover(S);
+        gotoxy(0, 0);
+        a.printarTabuleiro();
+    }
 
-    //b.rotacionar(c);
-
-    //b.printar(c);
-
-return 0;
+    return 0;
 }
