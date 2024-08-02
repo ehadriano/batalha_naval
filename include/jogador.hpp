de0b4a28c792fcffbaad0064725f70e5f148921a
Eduardo Hubner Adriano <ehadriano@ufmg.br>
2024-08-01 23:23:21 -0300

    Declara o método 'apagar'

--- include/jogador.hpp
+++ include/jogador.hpp
@@ -19,11 +19,12 @@ class jogador{ //Tentativa de tornar o código menor
     public:
 
         void printar(peca &);
+        void apagar( peca &);
         void rotacionar(peca &);
         coord rotacionar(coord &);
         void selecionar(peca &, int);
         void mover(peca &);
-        bool colisao(peca &); //Permite que a peça apenas se mova dentro do Tabuleiro
+        bool colisao(peca &);
 
 };
 
