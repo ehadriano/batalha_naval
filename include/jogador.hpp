6a3c2082928b86259a5d5f1ec93c5a0f55d8dfda
Eduardo Hubner Adriano <ehadriano@ufmg.br>
2024-08-02 11:14:37 -0300

    Declara uma nova variável no structure 'peca', chamada 'd' e realiza algumas mudanças nos paramêtros das funções

--- include/jogador.hpp
+++ include/jogador.hpp
@@ -1,7 +1,8 @@
 #ifndef JOGADOR_HPP
 #define JOGADOR_HPP
 
-//jogador.hpp
+using namespace std;
+
 void gotoxy(int x, int y){
 HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
  COORD dwPos;
@@ -22,6 +23,7 @@ struct peca{ //Peça
     coord original;
     coord perifericos [3];
     char D;
+    char d = '*';
     coord posicao(int n); //Posição
 
 };
@@ -31,13 +33,14 @@ class jogador{ //Tentativa de tornar o código menor
 
         void passarTabuleiro();
         void desenharMapaju();
-        void printar(peca &);
+        void printar(peca &, int);
         void apagar( peca &);
         void rotacionar(peca &);
         coord rotacionar(coord &);
         void selecionar(peca &, int);
-        void mover(peca &);
+        void mover(peca &, int &);
         bool colisao(peca &);
 
 };
+
 #endif // JOGADOR_HPP
