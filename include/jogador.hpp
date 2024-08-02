885d0aca0eb3ac808dc498bb4976ea59f060927c
Bicalhoz <118412355+Bicalhoz@users.noreply.github.com>
2024-08-02 18:00:50 -0300

    jogador.hpp
    Commit

--- include/jogador.hpp
+++ include/jogador.hpp
@@ -1,28 +1,34 @@
 #ifndef JOGADOR_HPP
 #define JOGADOR_HPP
 
-struct coord{ //Coordenadas
+//jogador.hpp
+struct coord { //Coordenadas
 
     int x, y;
 
 };
 struct peca{ //Peça
 
+struct peca { //Peça
     coord original;
-    coord perifericos [3];
+    coord perifericos[3];
     char D;
     coord posicao(int n); //Posição
 
 };
 class jogador{ //Tentativa de tornar o código menor
 
-    public:
+class jogador { //Tentativa de tornar o código menor
+public:
 
-        void printar(peca &);
-        void rotacionar(peca &);
-        coord rotacionar(coord &);
-        void selecionar(peca &, int);
+    void printar(peca &);
+    void limpar(peca &); //Limpa a posição anterior do navio
+    void rotacionar(peca &);
+    coord rotacionar(coord &);
+    void selecionar(peca &, int);
 
+    void mover(peca &);
+    bool dentroDoTabuleiro(peca &, char); //Garante que os navios só se possam se mover dentro do Tabuleiro
 };
 
 #endif // JOGADOR_HPP
