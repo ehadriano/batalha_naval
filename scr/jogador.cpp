7a07dab2e3bb83828de6ba8bdc41c8c05718c0a7
Eduardo Hubner Adriano <ehadriano@ufmg.br>
2024-08-01 23:24:01 -0300

    Implementa o método 'apagar'

--- scr/jogador.cpp
+++ scr/jogador.cpp
@@ -28,13 +28,20 @@ coord peca::posicao(int n){ //Posiciona um navio no tabuleiro
 return ret;
 }
 void jogador::printar(peca &P){
-
     for(int i=0; i<4; i++){ //Controla o número de casas que um navio pode ocupar
         coord c = P.posicao(i);
-        ma[c.y][c.x] = P.D;
+        ma[c.y][c.x] = P.D; // Marca a posição do navio com o caractere que representa o tipo do navio
+    }
+}
+void jogador::apagar(peca &P){ //Lógica identica ao método jogador::printar
+
+    for(int i=0; i<4; i++){
+        coord c = P.posicao(i);
+        ma[c.y][c.x] = ' '; // Substitui a posição do navio por um espaço em branco
     }
 
 }
+
 coord jogador::rotacionar(coord &c){
 
     coord ret ={c.y,c.x};
@@ -57,30 +64,33 @@ void jogador::selecionar(peca &P, int r){ //Inicia todos e cada um dos Navios
             P.perifericos[i] = peris[r][i];
     }
 }
-void jogador::mover(peca &P){ //Lógica parecida com a já implementada em cursor::moverCursor
-     if(kbhit()){
+void jogador::mover(peca &P) {
+    if (kbhit()) {
         peca copia = P;
         char tecla = getch();
-        if(tecla == 'a') P.original.x -=2;
+
+        // Processa a entrada do usuário para mover a peça
+       if(tecla == 'a') P.original.x -=2;
         if(tecla == 'd') P.original.x +=2;
         if(tecla == 'w') P.original.y -=2;
         if(tecla == 's') P.original.y +=2;
 
-        if(jogador::colisao(P)){
-            P = copia;
+        // Verifica se a nova posição causa uma colisão
+        if (jogador::colisao(P)) {
+            P = copia; // Reverte para a posição anterior em caso de colisão
+        } else {
+            // Atualiza o tabuleiro apenas se o movimento for válido
+            jogador::apagar(copia);
+            jogador::printar(P);
         }
-
-        jogador::printar(P);
-        }
+    }
 }
-bool jogador::colisao(peca &P){ //Lógica semelhante a função jogador::printar
-    for(int i=0; i<4; i++){
+bool jogador::colisao(peca &P) {
+    for (int i = 0; i < 4; i++) {
         coord c = P.posicao(i);
 
-        if(c.x < 2 || c.x > 20) return true;
-
-        if(c.y < 2 || c.y > 20) return true;
+        if (c.x < 2 || c.x > 20) return true;
+        if (c.y < 2 || c.y > 20) return true;
     }
-
-return false;
+    return false;
 }
