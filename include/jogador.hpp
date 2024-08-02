96142ae82448d1364bc74066151d24f71f90f0b7
Eduardo Hubner Adriano <ehadriano@ufmg.br>
2024-08-02 00:19:07 -0300

    Decalra 2 novos métodos, 'passarTabuleiro' e 'desenharMapaju'

--- include/jogador.hpp
+++ include/jogador.hpp
@@ -1,6 +1,17 @@
 #ifndef JOGADOR_HPP
 #define JOGADOR_HPP
 
+//jogador.hpp
+void gotoxy(int x, int y){
+HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+ COORD dwPos;
+ dwPos.X = x;
+ dwPos.Y = y;
+ SetConsoleCursorPosition(hcon, dwPos);
+}
+
+char ju[22][22];
+
 struct coord{ //Coordenadas
 
     int x, y;
@@ -18,6 +29,8 @@ class jogador{ //Tentativa de tornar o código menor
 
     public:
 
+        void passarTabuleiro();
+        void desenharMapaju();
         void printar(peca &);
         void apagar( peca &);
         void rotacionar(peca &);
@@ -27,5 +40,4 @@ class jogador{ //Tentativa de tornar o código menor
         bool colisao(peca &);
 
 };
-
 #endif // JOGADOR_HPP
