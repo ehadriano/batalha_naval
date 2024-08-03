112758bc01e99835d5d759822354a31920358497
Bicalhoz <118412355+Bicalhoz@users.noreply.github.com>
2024-08-03 15:39:57 -0300

    Testa as mudanças

--- scr/main.cpp
+++ scr/main.cpp
@@ -2,34 +2,20 @@
 #include <windows.h>
 #include <conio.h>
 #include "../include/cursor.hpp"
-
 using namespace std;
-
-/*Testa o método jogador::apagar e se a função de rotação
-está funcionado igual esperado*/
-int main() { 
-    cursor a;
-    jogador b;
 int main() {
-
     jogador a;
-
     peca S;
     int r = 0; //r=0 Carrier, r=1 Tanker, r=2 Destroyer, r=3 Submarine
-
-    b.selecionar(S, r);
     a.passarTabuleiro();
     a.selecionar(S, r);
-
     gotoxy(0, 0);
-    a.printarTabuleiro();
     a.desenharMapaju();
     while (true) {
-        b.mover(S);
         a.mover(S);
         gotoxy(0, 0);
-        a.printarTabuleiro();
         a.desenharMapaju();
+        a.mover(S,r);
     }
 
     return 0;
