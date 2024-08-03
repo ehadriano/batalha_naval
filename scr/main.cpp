71123431f2caadb7e5913eade0c77bfcd176c7bd
Bicalhoz <118412355+Bicalhoz@users.noreply.github.com>
2024-08-03 15:21:03 -0300

    Teste da colisão que substitui o metodo "dentrodotabuleiro"

--- scr/main.cpp
+++ scr/main.cpp
@@ -5,36 +5,8 @@
 
 using namespace std;
 
-int main(){
-
 int main() {
+int main() { //Testa o método 'colisao'
     cursor a;
     jogador b;
-
     peca S;
-    int r = 0; //r=0 Carrier, r=1 Tanker, r=2 Destroyer, r=3 Submarine
-
-    b.selecionar(S,r);
-    int r = 2; //r=0 Carrier, r=1 Tanker, r=2 Destroyer, r=3 Submarine
-
-    //gotoxy(0,0);
-    //a.printarTabuleiro();
-    b.selecionar(S, r);
-
-    b.printar(S);
-    gotoxy(0,25);
-    gotoxy(0, 0);
-    a.printarTabuleiro();
-    while (true) {
-        b.mover(S);
-        gotoxy(0, 0);
-        a.printarTabuleiro();
-    }
-
-    //b.rotacionar(c);
-
-    //b.printar(c);
-
-return 0;
-    return 0;
-}
