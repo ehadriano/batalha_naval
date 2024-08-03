7caf3bcb68e85a89f305c99cfcbc33cfacd9bbbc
Bicalhoz <118412355+Bicalhoz@users.noreply.github.com>
2024-08-03 15:26:18 -0300

    Declara 2 novos métodos, 'passarTabuleiro' e 'desenharMapaju'

--- include/jogador.hpp
+++ include/jogador.hpp
@@ -1,24 +1,32 @@
 #ifndef JOGADOR_HPP
 #define JOGADOR_HPP
+
+//jogador.hpp
+void gotoxy(int x, int y){
+HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+ COORD dwPos;
+ dwPos.X = x;
+ dwPos.Y = y;
+ SetConsoleCursorPosition(hcon, dwPos);
+}
+
+char ju[22][22];
+
 struct coord{ //Coordenadas
+
     int x, y;
-};
-struct peca{ //Peça
-    coord original;
-    coord perifericos [3];
-    char D;
-    coord posicao(int n); //Posição
-};
-class jogador{ //Tentativa de tornar o código menor
+@@ -18,6 +29,8 @@ class jogador{ //Tentativa de tornar o código menor
+
     public:
 
+        void passarTabuleiro();
+        void desenharMapaju();
         void printar(peca &);
         void apagar( peca &);
         void rotacionar(peca &);
-        coord rotacionar(coord &);
-        void selecionar(peca &, int);
-        void mover(peca &);
-        bool colisao(peca &); //Permite que a peça apenas se mova dentro do Tabuleiro
+@@ -27,5 +40,4 @@ class jogador{ //Tentativa de tornar o código menor
         bool colisao(peca &);
 
 };
+
+#endif // JOGADOR_HPP
