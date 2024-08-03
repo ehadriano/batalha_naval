7ac510a3fcf364e4f7027a5a32a6f825cee6c34b
Bicalhoz <118412355+Bicalhoz@users.noreply.github.com>
2024-08-03 15:31:13 -0300

    Testa os dois novos métodos

--- scr/main.cpp
+++ scr/main.cpp
@@ -5,24 +5,32 @@
 
 using namespace std;
 
-int main() { //Testa o método 'colisao'
 /*Testa o método jogador::apagar e se a função de rotação
 está funcionado igual esperado*/
 int main() { 
     cursor a;
     jogador b;
+int main() {
+
+    jogador a;
+
     peca S;
-    int r = 2; //r=0 Carrier, r=1 Tanker, r=2 Destroyer, r=3 Submarine
     int r = 0; //r=0 Carrier, r=1 Tanker, r=2 Destroyer, r=3 Submarine
 
     b.selecionar(S, r);
+    a.passarTabuleiro();
+    a.selecionar(S, r);
 
     gotoxy(0, 0);
     a.printarTabuleiro();
+    a.desenharMapaju();
     while (true) {
         b.mover(S);
+        a.mover(S);
         gotoxy(0, 0);
         a.printarTabuleiro();
+        a.desenharMapaju();
     }
+
     return 0;
 }
