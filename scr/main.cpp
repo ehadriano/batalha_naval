22f27b59f715a91080af86a2c7a66bd8031837ed
Bicalhoz <118412355+Bicalhoz@users.noreply.github.com>
2024-08-03 15:25:14 -0300

    Testa o método 'apagar' e a funcionalidade de rotação do navio

--- scr/main.cpp
+++ scr/main.cpp
@@ -5,8 +5,24 @@
 
 using namespace std;
 
-int main() {
 int main() { //Testa o método 'colisao'
+/*Testa o método jogador::apagar e se a função de rotação
+está funcionado igual esperado*/
+int main() { 
     cursor a;
     jogador b;
     peca S;
+    int r = 2; //r=0 Carrier, r=1 Tanker, r=2 Destroyer, r=3 Submarine
+    int r = 0; //r=0 Carrier, r=1 Tanker, r=2 Destroyer, r=3 Submarine
+
+    b.selecionar(S, r);
+
+    gotoxy(0, 0);
+    a.printarTabuleiro();
+    while (true) {
+        b.mover(S);
+        gotoxy(0, 0);
+        a.printarTabuleiro();
+    }
+    return 0;
+}
