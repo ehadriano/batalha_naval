0c2df5d67ff673e379cacbd97590173ae305a426
ehadriano <ehadriano@ufmg.br>
2024-08-06 11:13:32 -0300

    Delete scripts directory

--- scripts/create_shortcut.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set TARGET_PATH=%~dp0..\bin\main.exe
-set SHORTCUT_PATH=%~dp0..\Batalha_Naval.lnk
-set ICON_PATH=%~dp0..\img\ship.ico
-
-powershell -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut('%SHORTCUT_PATH%'); $s.TargetPath = '%TARGET_PATH%'; $s.IconLocation = '%ICON_PATH%'; $s.Save()"
