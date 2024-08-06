f14dd3d56e78d7c2652fe237f6f338814b2eb2de
Eduardo Hubner Adriano <ehadriano@ufmg.br>
2024-08-06 00:27:41 -0300

    Arquivo .hpp que define algumas constantes correspondentes aos códigos ASCII das teclas de direção do teclado.

---
+++ include/config.hpp
@@ -0,0 +1,11 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+// Define as constantes para as teclas de direção do teclado
+// Esses valores correspondem aos códigos ASCII das teclas de seta no modo de entrada de tecla do console
+#define ESQUERDA 75
+#define DIREITA 77
+#define CIMA 72
+#define BAIXO 80
+
+#endif // CONFIG_HPP
