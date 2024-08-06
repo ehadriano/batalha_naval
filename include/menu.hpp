e9309026d19d00ab2510eef138fe362e9fd46427
Eduardo Hubner Adriano <ehadriano@ufmg.br>
2024-08-06 00:43:22 -0300

    Declara os métodos da classe menu

---
+++ include/menu.hpp
@@ -0,0 +1,18 @@
+#ifndef MENU_HPP
+#define MENU_HPP
+
+// Declaração da classe `menu`
+// Esta classe contém métodos para exibir diferentes telas do menu do jogo
+class menu {
+public:
+    // Método para exibir a tela inicial do jogo
+    void TELA_INICIAL();
+
+    // Método para exibir as instruções do jogo
+    void INSTRUCOES();
+
+    // Método para exibir o objetivo do jogo
+    void OBJETIVO();
+};
+
+#endif // MENU_HPP
