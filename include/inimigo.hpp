2aea89f15a728e6a3a671643d131a7bca9f11d2c
juliammelo06 <menezesmelojulia@gmail.com>
2024-08-05 21:59:59 -0300

    Declara os métodos 'colisao' e 'mover'

--- include/inimigo.hpp
+++ include/inimigo.hpp
@@ -1,24 +1,21 @@
 #ifndef INIMIGO_HPP
 #define INIMIGO_HPP
-
 #include <windows.h>
-
-// Função para mover o cursor no console para uma posição específica
+#include "jogador.hpp"
+//inimigo.hpp
 void gotoxy(int x, int y){
-    HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD dwPos;
-    dwPos.X = x;
-    dwPos.Y = y;
-    SetConsoleCursorPosition(hcon, dwPos);
+HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+ COORD dwPos;
+ dwPos.X = x;
+ dwPos.Y = y;
+ SetConsoleCursorPosition(hcon, dwPos);
 }
-
 struct coord{ //Coordenadas
     int x, y;
 };
-
 struct peca{ //Peça
     coord original;
-    coord perifericos[3];
+    coord perifericos [3];
     char D;
     char d = '*';
     coord posicao(int n); //Posição
@@ -26,14 +23,26 @@ struct peca{ //Peça
 
 char en[22][22];
 
-class inimigo{ //Métodos semelhantes a classe jogador
+class inimigo : public jogador {
+public:
+    void desenharMapaen();
+    void printar(peca &p, int r);
+    void apagar(peca &p);
+    void mover(peca &p, int &r);
+    bool colisao(peca &p);
+    void selecionaMapa(peca &p, int r);
+    void inicializarNavios(peca &p);
+    int obterRot();
+class inimigo : public jogador{
     public:
         void desenharMapaen();
         void printar(peca &, int);
         void apagar(peca &);
-        void selecionar(peca &, int);
-        void mover(peca &, int &);
-        bool colisao(peca &);
+        void mover(peca &, int &, int &);
+        bool colisao(peca &, int);
+        void selecionaMapa(peca &, int);
+        void inicializarNavios(peca &);
+        int obterRot();
 };
 
 #endif // INIMIGO_HPP
