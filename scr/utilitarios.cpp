b529c75924124e991ec1ab26937f6ef3355a7af6
Eduardo Hubner Adriano <ehadriano@ufmg.br>
2024-08-06 00:46:48 -0300

    Implementa as funções declaradas em utilitarios.hpp

---
+++ scr/utilitarios.cpp
@@ -0,0 +1,21 @@
+#include "../include/utilitarios.hpp"
+#include <conio.h>
+
+//Funções auxiliares
+
+void gotoxy(int x, int y) {
+    HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD dwPos;
+    dwPos.X = x;
+    dwPos.Y = y;
+    SetConsoleCursorPosition(hcon, dwPos);
+}
+
+void ocultarCursor() {
+    HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cci;
+    cci.dwSize = 1;
+    cci.bVisible = FALSE;
+    SetConsoleCursorInfo(hcon, &cci);
+}
+
