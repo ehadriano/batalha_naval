29248f875d4a79e88af4acf858b9014e6c1eb6c7
juliammelo06 <menezesmelojulia@gmail.com>
2024-08-05 22:27:49 -0300

    Testas as mudanças

--- scr/main.cpp
+++ scr/main.cpp
@@ -1,43 +1,51 @@
+#include "../include/jogador.hpp"
+#include "../include/inimigo.hpp"
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
-#include "../include/cursor.hpp"
-#include "../include/jogador.hpp"
-#include "../include/inimigo.hpp"
-
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
-
+void gotoxy(int x, int y) {
+    HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD dwPos;
+    dwPos.X = x;
+    dwPos.Y = y;
+    SetConsoleCursorPosition(hcon, dwPos);
+}
 int main() {
+    srand(time(NULL));
     jogador a;
     inimigo b;
 
-    peca S;
+    peca S, S1;
     int r = 0; // r=0 Carrier, r=1 Tanker, r=2 Destroyer, r=3 Submarine
+    int r = 0; //r=0 Carrier, r=1 Tanker, r=2 Destroyer, r=3 Submarine
     int t = 0;
 
     a.passarTabuleiro();
     a.selecionar(S, r);
 
-    gotoxy(0, 0);
-    a.desenharMapaju();
-    gotoxy(0, 25);
-    b.desenharMapaen();
+    b.inicializarNavios(S1);
+
+    gotoxy(0, 0); a.desenharMapaju();
+    gotoxy(0, 25); b.desenharMapaen();
 
     while (true) {
-        gotoxy(0, 0);
-        a.desenharMapaju();
-        a.mover(S, r);
-        gotoxy(0, 0);
-        a.desenharMapaju();
-        gotoxy(0, 25);
-        b.desenharMapaen();
+    while(true){
+        gotoxy(0, 0); a.desenharMapaju();
+        gotoxy(0, 25); b.desenharMapaen();
 
         if (t == 0) {
+        if(t == 0){
             a.mover(S, r);
             if (r == 4) t = 1;
+            if(r == 4) t = 1;
         }
         if (t == 1) {
-            b.mover(S, r);
+            b.mover(S1, r);
+        if(t == 1){
+            b.mover(S1, r, t);
         }
     }
 
