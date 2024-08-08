63ddcc56983d5081b66a01c144b8372ef7e9e9f3
ehadriano <ehadriano@ufmg.br>
2024-08-08 18:14:13 -0300

    Versão Final

--- include/config.hpp
+++ include/config.hpp
@@ -1,11 +1,36 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
-// Define as constantes para as teclas de direção do teclado
-// Esses valores correspondem aos códigos ASCII das teclas de seta no modo de entrada de tecla do console
+/**
+ * @file config.hpp
+ * @brief Define as constantes para as teclas de direção do teclado.
+ *
+ * @brief Este arquivo contém as definições dos códigos ASCII das teclas de seta,
+ * @brief utilizados para controlar a navegação no jogo de batalha naval.
+ */
+
+/**
+ * @def ESQUERDA
+ * @brief Código ASCII para a tecla de seta para a esquerda.
+ */
 #define ESQUERDA 75
+
+/**
+ * @def DIREITA
+ * @brief Código ASCII para a tecla de seta para a direita.
+ */
 #define DIREITA 77
+
+/**
+ * @def CIMA
+ * @brief Código ASCII para a tecla de seta para cima.
+ */
 #define CIMA 72
+
+/**
+ * @def BAIXO
+ * @brief Código ASCII para a tecla de seta para baixo.
+ */
 #define BAIXO 80
 
 #endif // CONFIG_HPP
