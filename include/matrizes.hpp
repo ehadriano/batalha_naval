12fcd286674e47ee2684e4e8bbd515ca641122c7
ehadriano <ehadriano@ufmg.br>
2024-08-08 18:16:55 -0300

    Versão Final

--- include/matrizes.hpp
+++ include/matrizes.hpp
@@ -1,11 +1,29 @@
 #ifndef MATRIZES_HPP
 #define MATRIZES_HPP
 
-// Declaração de matrizes globais extern
-// Essas matrizes são definidas em outro arquivo de implementação e são usadas para armazenar o estado do jogo
+/**
+ * @file matrizes.hpp
+ * @brief Declaração de matrizes globais extern.
+ *
+ * Essas matrizes são definidas em outro arquivo de implementação e são usadas para armazenar o estado do jogo.
+ */
 
-extern char MA[22][22]; // Matriz MA para armazenar o estado do tabuleiro principal
-extern char JO[22][22]; // Matriz JO para armazenar o estado do tabuleiro do jogador
-extern char en[22][22]; // Matriz en para armazenar o estado do tabuleiro do inimigo
+/**
+ * @var MA
+ * @brief Matriz MA para armazenar o estado do tabuleiro principal.
+ */
+extern char MA[22][22];
+
+/**
+ * @var JO
+ * @brief Matriz JO para armazenar o estado do tabuleiro do jogador.
+ */
+extern char JO[22][22];
+
+/**
+ * @var en
+ * @brief Matriz en para armazenar o estado do tabuleiro do inimigo.
+ */
+extern char en[22][22];
 
 #endif // MATRIZES_HPP
