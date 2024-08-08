e04f57f23a01a6c7e87e34e016aab36a3df436b0
ehadriano <ehadriano@ufmg.br>
2024-08-08 18:17:18 -0300

    Versão Final

--- include/menu.hpp
+++ include/menu.hpp
@@ -1,17 +1,34 @@
 #ifndef MENU_HPP
 #define MENU_HPP
 
-// Declaração da classe `menu`
-// Esta classe contém métodos para exibir diferentes telas do menu do jogo
+/**
+ * @file menu.hpp
+ * @brief Declaração da classe menu.
+ *
+ * Esta classe contém métodos para exibir diferentes telas do menu do jogo.
+ */
+
+/**
+ * @class menu
+ * @brief Classe para exibição do menu do jogo.
+ *
+ * A classe menu contém métodos para exibir a tela inicial, as instruções e o objetivo do jogo.
+ */
 class menu {
 public:
-    // Método para exibir a tela inicial do jogo
+    /**
+     * @brief Método para exibir a tela inicial do jogo.
+     */
     void TELA_INICIAL();
 
-    // Método para exibir as instruções do jogo
+    /**
+     * @brief Método para exibir as instruções do jogo.
+     */
     void INSTRUCOES();
 
-    // Método para exibir o objetivo do jogo
+    /**
+     * @brief Método para exibir o objetivo do jogo.
+     */
     void OBJETIVO();
 };
 
