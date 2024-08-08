77f71f44c11cb28be9e53069896a3de81e90dba3
ehadriano <ehadriano@ufmg.br>
2024-08-08 18:22:15 -0300

    Versão Final

--- scr/utilitarios.cpp
+++ scr/utilitarios.cpp
@@ -1,8 +1,16 @@
 #include "../include/utilitarios.hpp"
 #include <conio.h>
 
-//Funções auxiliares
-
+/**
+ * @file utilitarios.cpp
+ * @brief Implementação de funções utilitárias.
+ */
+/**
+ * @brief Move o cursor para a posição especificada no console.
+ *
+ * @param x Coordenada x para onde o cursor deve ser movido.
+ * @param y Coordenada y para onde o cursor deve ser movido.
+ */
 void gotoxy(int x, int y) {
     HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
     COORD dwPos;
@@ -11,6 +19,11 @@ void gotoxy(int x, int y) {
     SetConsoleCursorPosition(hcon, dwPos);
 }
 
+/**
+ * @brief Oculta o cursor no console.
+ *
+ * Torna o cursor do console invisível para melhorar a aparência durante a execução do jogo.
+ */
 void ocultarCursor() {
     HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO cci;
@@ -18,4 +31,3 @@ void ocultarCursor() {
     cci.bVisible = FALSE;
     SetConsoleCursorInfo(hcon, &cci);
 }
-
